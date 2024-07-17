5a29e632cb6ab7ddf06f2b4a8374680585aaac34
Dhruv Chandra <Dhruv.Chandra@sirva.com>
2024-07-17 13:31:39 -0400

    Initial commit

---
+++ src/TodoAPI.UnitTests/UnitTest1.cs
@@ -0,0 +1,52 @@
+using Xunit;
+using TodoApi.Models;
+
+namespace TodoApi.UnitTests
+{
+    public class TodoItemUnitTests
+    {
+        //[Fact]
+        //public void IsComplete_DefaultValue_ShouldBeFalse()
+        //{
+        //    // Arrange
+        //    var todoItem = new TodoItem();
+
+        //    // Act & Assert
+        //    Assert.False(todoItem.IsComplete);
+        //}
+        [Fact]
+        public void IsComplete_DefaultValue_ShouldBeTrue()
+        {
+            // Arrange
+            var todoItem = new TodoItem();
+            todoItem.IsComplete = true;
+
+            // Act & Assert
+            Assert.True(todoItem.IsComplete);
+        }
+        [Fact]
+        public void Name_Value_IsNotNull()
+        {
+            // Arrange
+            var todoItem = new TodoItem();
+            todoItem.Name = "Complete ASP.NET Project Work";
+
+            // Act & Assert
+            //Assert.NotNull(todoItem.Name);
+            Assert.NotEmpty(todoItem.Name);
+        }
+        [Fact]
+        public void Name_Value_IsEmpty()
+        {
+            // Arrange
+            var todoItem = new TodoItem();
+            todoItem.Name = String.Empty;
+
+            // Act & Assert
+            //Assert.NotNull(todoItem.Name);
+            Assert.Empty(todoItem.Name);
+
+        }
+
+    }
+}
